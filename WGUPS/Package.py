11f6d72ceb7e8682d2196e5bdea37eaba13ae254
Nick King <nickking93@live.com>
2024-04-19 11:38:41 -0500

    Defines the Package and Truck classes; adds "CreateHashTable.py"

--- WGUPS/Package.py
+++ WGUPS/Package.py
@@ -1,2 +1,33 @@
-#Package Class
+# Define Package Class
+class Package:
 
+    def __init__(self, id, address, city, state, zipcode, delivery_deadline, weight, status):
+        self.id = id
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zipcode = zipcode
+        self.delivery_deadline = delivery_deadline
+        self.weight = weight
+        self.status = status
+        self.departure_time = None
+        self.delivery_time = None
+
+    # Overloaded string conversion method to create a string representation of the attributes
+    def __str__(self):
+
+        return "%s, %s, %s, %s, %s, %s, %s, %s, %s" % (self.id, self.address, self.city, self.state, self.zipcode,
+                                                       self.delivery_deadline, self.weight, self.delivery_time,
+                                                       self.status)
+
+    # Set delivery status based on delivery time
+    def get_status(self, elapsed_time):
+
+        if self.delivery_time < elapsed_time:
+            self.status = "Delivered"
+
+        elif self.departure_time > elapsed_time:
+            self.status = "En route"
+
+        else:
+            self.status = "At hub"
--- WGUPS/main.py
+++ WGUPS/main.py
@@ -1,3 +1,9 @@
-#C950 - WGUPS
-#Code By: Robert King
-#Student ID: 011133982
+# C950 - WGUPS
+# Code By: Robert King
+# Student ID: 011133982
+
+# Import classes and data files
+import Truck
+import Package
+import CSV
+import datetime
