bf064bffa22420960672186aa53d597f1585d037
Nick King <nickking93@live.com>
2024-04-20 12:39:10 -0500

    Adds method that implements nearest neighbor algorithm to sort packages in "main.py"

--- WGUPS/main.py
+++ WGUPS/main.py
@@ -38,7 +38,7 @@ with open("CSV/WGUPS_Packages.csv") as dataFile2:
 def add_packages(filename, hash_table):
     with open(filename) as package_file:
         package_data = csv.reader(package_file)
-        next(package_data) # Skip first line to remove header
+        next(package_data)  # Skip first line to remove header
         for package in package_data:  # Set attributes
             pack_id = package[0]
             pack_address = package[1]
@@ -73,20 +73,59 @@ def parse_address(address):
             return int(r[0])
 
 
-# Create the 3 truck objects with default attributes based on assignment assumptions and supporting data
+# Create the 3 truck objects with default attributes based on assignment assumptions and package special notes
 truck1 = Truck.Truck(16, 18, None,
-                     [1, 13, 14, 15, 16, 20, 29, 30, 31, 34, 37, 40], 0.0,
+                     [1, 13, 14, 15, 16, 20, 29, 30, 31, 33, 34], 0.0,
                      "4001 South 700 East", datetime.timedelta(hours=8))
 
 truck2 = Truck.Truck(16, 18, None,
-                     [3, 6, 12, 17, 18, 19, 21, 22, 23, 24, 26, 27, 35, 36, 38, 39], 0.0,
+                     [3, 6, 12, 17, 18, 19, 21, 22, 23, 24, 26, 27, 35, 36, 38], 0.0,
                      "4001 South 700 East", datetime.timedelta(hours=10, minutes=20))
 
 truck3 = Truck.Truck(16, 18, None,
-                     [2, 4, 5, 6, 7, 8, 9, 10, 11, 25, 28, 32, 33], 0.0,
+                     [2, 4, 5, 6, 7, 8, 9, 10, 11, 25, 28, 32, 37, 39, 40], 0.0,
                      "4001 South 700 East", datetime.timedelta(hours=9, minutes=5))
 
 # Create hash table and load in packages
 hash_Table = CreateHashMap()
 add_packages("CSV/WGUPS_Packages.csv", hash_Table)
 
+
+# Method to assign trucks with packages using nearest neighbor
+def assign_packages(truck):
+    unsorted = []
+
+    # Move packages into a list of unsorted packages to sort and assign them
+    for idNum in truck.packages:
+        package = hash_Table.ht_search(idNum)
+        unsorted.append(package)
+    truck.packages.clear()
+
+    # Find nearest package to truck and insert back into truck's package array
+    while len(unsorted) > 0:
+        destination = 2000
+        next_package = None
+
+        # Check the distance between truck and each package and determine next package
+        for package in unsorted:
+            if calculate_distance(parse_address(truck.address), parse_address(package.address)) <= destination:
+                destination = calculate_distance(parse_address(truck.address), parse_address(package.address))
+                next_package = package
+
+        # Assign next package to truck and remove from unsorted list
+        truck.packages.append(next_package.ID)
+        unsorted.remove(next_package)
+
+        truck.totalMiles += destination  # Add mileage from each truck to the trucks total mileage
+        truck.address = next_package.addres  # Update trucks starting location to package location in each iteration
+
+        # Update time for truck and next package
+        truck.time += datetime.timedelta(hours=destination / 18)
+        destination.delivery_time = truck.time
+        destination.departure_time = truck.depart_time
+
+
+assign_packages(truck1)
+assign_packages(truck2)
+truck3.depart_time = min(truck1.time, truck2.time)
+assign_packages(truck3)
